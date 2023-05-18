1b718df5fc6ee618ea2886328cfa749c1b66e1ec
Henate <Henate@126.com>
2023-05-18 16:07:39 +0800

    update batch transfer with UsrOps

--- test/Simple4337.test.ts
+++ test/Simple4337.test.ts
@@ -7,6 +7,8 @@ import {
   SimpleAccount__factory,
   TestCounter,
   TestCounter__factory,
+  TestToken__factory,
+  TestToken,
 } from '../typechain-types'
 import {
   createAccount,
@@ -16,9 +18,55 @@ import {
   rethrow,
   simulationResultCatch,
 } from './testutils'
-import { PopulatedTransaction, Wallet } from 'ethers'
+import { PopulatedTransaction, Signer, Wallet } from 'ethers'
 import { expect } from 'chai'
 import { fillAndSign } from './UserOp'
+import { UserOperationStruct } from 'userop/dist/typechain/EntryPoint'
+
+async function generateBatchofERC20TransferOp(
+  signer: Signer,
+  token: TestToken,
+  entryPoint: EntryPoint,
+  adminAccount: SimpleAccount,
+  testLoop: number
+) {
+  let erc20TransfercallData: PopulatedTransaction
+  let account: SimpleAccount
+  //const signer = ethers.provider.getSigner()
+  const accountOwner = createAccountOwner()
+
+  const transfercallData = await token.populateTransaction.transfer(
+    accountOwner.address,
+    100 * (testLoop + 1)
+  )
+
+  erc20TransfercallData = await adminAccount.populateTransaction.execute(
+    token.address,
+    0,
+    transfercallData.data!
+  )
+  ;({ proxy: account } = await createAccount(
+    signer,
+    await accountOwner.getAddress(),
+    entryPoint.address
+  ))
+
+  await fund(account.address)
+  await token.mint(account.address, 1000)
+
+  const op = await fillAndSign(
+    {
+      callData: erc20TransfercallData.data,
+      sender: account.address,
+      callGasLimit: 2e6,
+      verificationGasLimit: 2e6,
+    },
+    accountOwner,
+    entryPoint
+  )
+  console.log('loop:', testLoop, 'toOwner:', accountOwner.address)
+  return { op, accountOwner, account }
+}
 
 describe('SimpleAccount', () => {
   let entryPoint: EntryPoint
@@ -161,5 +209,168 @@ describe('SimpleAccount', () => {
 
       expect(await counter.counters(account2.address)).to.equal(1)
     })
+  })
+})
+
+describe('handle ERC20 token with 4337 solution', () => {
+  let entryPoint: EntryPoint
+  const ethersSigners = ethers.provider.getSigner()
+  let accountOwner: Wallet
+  let account: SimpleAccount
+  let token: TestToken
+  let beneficiaryAddress: string
+  let simpleAccountFactory: SimpleAccountFactory
+
+  before(async () => {
+    accountOwner = createAccountOwner()
+    beneficiaryAddress = createAddress()
+    entryPoint = await new EntryPoint__factory(ethersSigners).deploy()
+    account = await new SimpleAccount__factory(ethersSigners).deploy(
+      entryPoint.address
+    )
+    token = await new TestToken__factory(ethersSigners).deploy()
+  })
+
+  it('handle ERC20 mint Op', async () => {
+    let execcallData: PopulatedTransaction
+    let ERCaccount: SimpleAccount
+
+    const mintcallData = await token.populateTransaction.mint(
+      beneficiaryAddress,
+      1000
+    )
+
+    execcallData = await account.populateTransaction.execute(
+      token.address,
+      0,
+      mintcallData.data!
+    )
+    ;({ proxy: ERCaccount } = await createAccount(
+      ethersSigners,
+      await accountOwner.getAddress(),
+      entryPoint.address
+    ))
+
+    console.log('AA deployed to:', ERCaccount.address)
+
+    await fund(ERCaccount.address)
+
+    const op = await fillAndSign(
+      {
+        callData: execcallData.data,
+        sender: ERCaccount.address,
+        callGasLimit: 2e6,
+        verificationGasLimit: 1e6,
+      },
+      accountOwner,
+      entryPoint
+    )
+
+    await entryPoint.callStatic
+      .simulateValidation(op, { gasPrice: 1e9 })
+      .catch(simulationResultCatch)
+
+    const tx = await entryPoint
+      .handleOps([op], accountOwner.address)
+      .catch(rethrow())
+      .then(async (r) => r!.wait())
+
+    console.log('mint gasused:', tx.gasUsed.toString())
+    const balance = await token.balanceOf(beneficiaryAddress)
+    expect(balance).to.equal(1000)
+    console.log('mint benefi address balance:', balance.toString())
+  })
+
+  it('handle ERC20 transfer Op for one time', async () => {
+    let erc20TransfercallData: PopulatedTransaction
+    let ercAccount: SimpleAccount
+
+    const transfercallData = await token.populateTransaction.transfer(
+      beneficiaryAddress,
+      100
+    )
+
+    erc20TransfercallData = await account.populateTransaction.execute(
+      token.address,
+      0,
+      transfercallData.data!
+    )
+    ;({ proxy: ercAccount } = await createAccount(
+      ethersSigners,
+      await accountOwner.getAddress(),
+      entryPoint.address
+    ))
+    console.log('AA account deployed to:', ercAccount.address)
+
+    await fund(ercAccount.address)
+    await token.mint(ercAccount.address, 500)
+
+    const op = await fillAndSign(
+      {
+        callData: erc20TransfercallData.data,
+        sender: ercAccount.address,
+        callGasLimit: 2e6,
+        verificationGasLimit: 1e6,
+      },
+      accountOwner,
+      entryPoint
+    )
+
+    const tx = await entryPoint
+      .handleOps([op], accountOwner.address)
+      .then(async (r) => r!.wait())
+
+    console.log('transfer gasused:', tx.gasUsed.toString())
+    const balance = await token.balanceOf(beneficiaryAddress)
+    console.log('transfer benefi address balance:', balance.toString())
+    expect(balance).to.equal(1100)
+  })
+
+  it('handle batch of ERC20 transfer Ops', async () => {
+    const testLoopLimit = 10
+    const ops: UserOperationStruct[] = []
+    const ercAccounts: SimpleAccount[] = []
+    const accountOwners: Wallet[] = []
+    let ercAccount: SimpleAccount
+
+    for (let testLoop = 0; testLoop < testLoopLimit; testLoop++) {
+      const { op, accountOwner } = await generateBatchofERC20TransferOp(
+        ethersSigners,
+        token,
+        entryPoint,
+        account,
+        testLoop
+      )
+
+      ops.push(op)
+
+      accountOwners.push(accountOwner)
+    }
+
+    console.log('op', ops[0])
+
+    const tx = await entryPoint
+      .handleOps(ops, accountOwner.address, {
+        maxFeePerGas: 1e9,
+        maxPriorityFeePerGas: 1e9,
+      })
+      .then(async (t) => await t.wait())
+
+    console.log(
+      'batch transfer gasused:',
+      tx.gasUsed.toString(),
+      'avgGas:',
+      tx.gasUsed.div(testLoopLimit).toString()
+    )
+    for (let testloop = 0; testloop < testLoopLimit; testloop++) {
+      const balance = await token.balanceOf(accountOwners[testloop].address)
+      console.log(
+        'account:',
+        accountOwners[testloop].address,
+        'balance:',
+        balance.toString()
+      )
+      expect(balance).to.equal((testloop + 1) * 100)
+    }
   })
 })
